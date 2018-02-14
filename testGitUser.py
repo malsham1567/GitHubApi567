86f128b1d7651433f6487960abf5989578b5d9b7
malsham1567 <35948110+malsham1567@users.noreply.github.com>
2018-02-14 15:54:27 -0500

    Update testGitUser.py

--- testGitUser.py
+++ testGitUser.py
@@ -3,7 +3,7 @@ import unittest
 from GetHub_user import get_reps
 
 
-class TestGetReps(unittest.TestCase):
+class testGitUser(unittest.TestCase):
     def testGetRepos(self):
         self.assertEqual(get_reps('m6ni'), ['Repo: homework9- Number of commits: 2', 'Repo: home_work9 Number of commits: 7', 'Repo: homwork9_1 Number of commits: 2'])
         self.assertEqual(get_reps('talal3456'), 'Invalid user')
