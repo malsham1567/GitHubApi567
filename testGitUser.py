1853bf28c272eb85268403709fc08b5903c6e310
malsham1567 <35948110+malsham1567@users.noreply.github.com>
2018-03-09 04:22:40 -0500

    Update testGitUser.py

--- testGitUser.py
+++ testGitUser.py
@@ -4,7 +4,7 @@ from GetHub_user import get_reps
 
 
 class TestGetReps(unittest.TestCase):
-    @mock.patch('GetHub_user.get_info')
+    @mock.patch('GetHub_user.get_reps')
     def testGetRepos(self,mockedReq):
         mockedReq.return_value = ['Repo: homework9- has  0 commits', 'Repo: home_work9 has  7 commits', 'Repo: homwork9_1 has  2 commits']
         self.assertEqual(get_reps('m6ni'), ['Repo: homework9- has  0 commits', 'Repo: home_work9 has  7 commits', 'Repo: homwork9_1 has  2 commits'])
